98c7d9c3a44068d0863288963a3fc6eef884eb4b
carlosdelgadillo10 <carlos.delgadillo102003@gmail.com>
2024-06-21 12:25:42 -0600

    commit inicial

---
+++ suma.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class Operacion(BaseModel):
+    num1: float
+    num2: float
+
+# Endpoint para la operación de suma
+@app.post("/sumar")
+def sumar(op: Operacion):
+    return op.num1 + op.num2
+
+@app.post("/restar")
+def restar(op: Operacion):
+    return op.num1 - op.num2
